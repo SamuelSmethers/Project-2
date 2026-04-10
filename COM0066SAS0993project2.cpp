da659af862e175fbb170ae25eff9ac5ee5da65d7
CristianMireles <crimireles@gmail.com>
2026-04-10 20:25:15 +0000

    made changes

--- COM0066SAS0993project2.cpp
+++ COM0066SAS0993project2.cpp
@@ -91,13 +91,18 @@ int main()
 		cout<<"Enter 1-3:";
 		cin>>accountTypeSelection;
 		cout<<endl;
+		if(isalpha(accountTypeSelection))
+		{
+			cout << "Do not enter a letter or special characters" << endl;
+			accountTypeSelection = 0;
+		}
 	}while(!(accountTypeSelection>0 && accountTypeSelection<4));
 
 	cout << "+-----------------------------------------------------+"<< endl;
 	cout<< "Account holder: " << name << endl;
 	cout<<"Account Type: "<<endl;
 	cout<<"Balance: "<<balance<<endl;
-	cout << "+-----------------------------------------------------+"<< endl;
+	
 
 
 	int menuSelection;
